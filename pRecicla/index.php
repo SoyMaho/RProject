7a76c29b28c635bd6548c3a716377faf112116fc
logicalsolid0 <61362070+logicalsolid0@users.noreply.github.com>
2020-03-17 17:53:38 -0600

    Se agrega Codigo iniciar

---
+++ pRecicla/index.php
@@ -0,0 +1,36 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Reciclare</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Sen&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/grid.css">
+  </head>
+  <body>
+
+    <div class="header">
+      <h1>CSS GRID</h1>
+    </div>
+
+    <div class="contenedor">
+
+    <div class="item">1</div>
+    <div class="">2</div>
+    <div class="">3</div>
+    <div class="">4</div>
+    <div class="">5</div>
+    <div class="">6</div>
+    <div class="">7</div>
+    <div class="">8</div>
+    <div class="">9</div>
+    <div class="">10</div>
+    <div class="">11</div>
+    <div class="">12</div>
+
+    </div>
+
+  </body>
+</html>
